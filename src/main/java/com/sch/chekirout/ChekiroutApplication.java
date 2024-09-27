3b8174fff15b414c9171d859973ae6f8baa0f315
Ogu1208 <kdasunb6@gmail.com>
2024-09-27 14:08:07 +0900

    chore: 초기 프로젝트 설정

---
+++ src/main/java/com/sch/chekirout/ChekiroutApplication.java
@@ -0,0 +1,13 @@
+package com.sch.chekirout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChekiroutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChekiroutApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sch/chekirout/ChekiroutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sch.chekirout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChekiroutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
