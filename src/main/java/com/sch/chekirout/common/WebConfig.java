24a5ac91c44794ad636dea7ce0e781b76fb49593
Ogu1208 <kdasunb6@gmail.com>
2024-10-01 22:43:00 +0900

    chore: cors설정을 위한 WebMvcConfig

---
+++ src/main/java/com/sch/chekirout/common/WebConfig.java
@@ -0,0 +1,22 @@
+package com.sch.chekirout.common;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                .allowedHeaders("Authorization", "Content-Type")
+                .exposedHeaders("Custom-Header")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+
+}
