75bc5b3c57e674e9374842bf642f496bfc5cc191
Ogu1208 <kdasunb6@gmail.com>
2024-10-01 15:32:23 +0900

    chore: swaggerConfig 추가

--- src/main/java/com/sch/chekirout/common/SwaggerConfig.java
+++ src/main/java/com/sch/chekirout/common/SwaggerConfig.java
@@ -1,2 +1,24 @@
-package com.sch.chekirout.common;public class SwaggerConfig {
+package com.sch.chekirout.common;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Chekirout API")
+                .description("2024SCH 나만의학술제 출첵앱 - Chekirout API")
+                .version("1.0.0");
+    }
 }
