87c60bbc711fa26e2dca59f9289fa088224477d7
Ogu1208 <kdasunb6@gmail.com>
2024-10-02 13:05:41 +0900

    refactor: category id생성 방식 수정, @EnableJpaUuditing 추가

--- src/main/java/com/sch/chekirout/category/domain/Category.java
+++ src/main/java/com/sch/chekirout/category/domain/Category.java
@@ -20,7 +20,7 @@ import java.util.List;
 public class Category extends BaseEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false, unique = true)
