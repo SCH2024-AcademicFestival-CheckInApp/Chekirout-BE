3f7ac2ddc89c72a5faefda1dcc7801b8d22f0cc0
tejava7177 <wngmstla@sch.ac.kr>
2024-10-03 15:31:01 +0900

    Auto stash before checking out "origin/feature/securtiy"

--- src/main/java/com/sch/chekirout/auth/jwt/config/JwtProperties.java
+++ src/main/java/com/sch/chekirout/auth/jwt/config/JwtProperties.java
@@ -1,14 +1,24 @@
 package com.sch.chekirout.auth.jwt.config;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+
 @ConfigurationProperties(prefix = "jwt")
 public class JwtProperties {
 
     private String issuer;
     private String secret_key;
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Issuer: " + issuer);
+        System.out.println("Secret Key: " + secret_key);
+    }
+
 
     // Getter & Setter
     public String getIssuer() {
