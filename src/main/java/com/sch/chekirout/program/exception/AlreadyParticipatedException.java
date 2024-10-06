11f74c14bb724e40384b1821bac4cd70a4bfd40d
Ogu1208 <kdasunb6@gmail.com>
2024-10-06 22:59:21 +0900

    fix: CustomException 상속 클래스 오류 지정 수정

--- src/main/java/com/sch/chekirout/program/exception/AlreadyParticipatedException.java
+++ src/main/java/com/sch/chekirout/program/exception/AlreadyParticipatedException.java
@@ -1,6 +1,8 @@
 package com.sch.chekirout.program.exception;
 
-public class AlreadyParticipatedException extends RuntimeException {
+import com.sch.chekirout.common.exception.CustomBadRequestException;
+
+public class AlreadyParticipatedException extends CustomBadRequestException {
     public AlreadyParticipatedException(final String message) {
         super(String.format("이미 참여한 프로그램입니다. 프로그램 이름 : %s", message));
     }
--- src/main/java/com/sch/chekirout/program/exception/DistanceOutOfRangeException.java
+++ src/main/java/com/sch/chekirout/program/exception/DistanceOutOfRangeException.java
@@ -1,6 +1,8 @@
 package com.sch.chekirout.program.exception;
 
-public class DistanceOutOfRangeException extends RuntimeException {
+import com.sch.chekirout.common.exception.CustomBadRequestException;
+
+public class DistanceOutOfRangeException extends CustomBadRequestException {
     public DistanceOutOfRangeException() {
         super("거리 허용 범위를 벗어난 위치입니다.");
     }
