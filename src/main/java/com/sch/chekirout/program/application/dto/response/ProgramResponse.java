783df427e35f1ff08cad8eb554dacbed7a725ec3
Ogu1208 <kdasunb6@gmail.com>
2024-10-07 13:29:41 +0900

    refactor: programResponse를 record로 변경

--- src/main/java/com/sch/chekirout/program/application/dto/response/ProgramResponse.java
+++ src/main/java/com/sch/chekirout/program/application/dto/response/ProgramResponse.java
@@ -10,14 +10,33 @@ import lombok.Getter;
 import java.time.LocalDateTime;
 
 @Builder
-public record ProgramResponse(@Schema(description = "프로그램 ID") String id,
-                              @Schema(description = "프로그램 이름") String name,
-                              @Schema(description = "프로그램 설명") String description,
-                              @Schema(description = "카테고리 ID") Long categoryId,
-                              @Schema(description = "카테고리 이름") String categoryName,
-                              @Schema(description = "프로그램 시작 시간") @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd HH:mm") LocalDateTime startTimestamp,
-                              @Schema(description = "프로그램 종료 시간") @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd HH:mm") LocalDateTime endTimestamp,
-                              @Schema(description = "알림 여부") boolean notificationYn) {
+public record ProgramResponse(
+        @Schema(description = "프로그램 ID")
+        String id,
+
+        @Schema(description = "프로그램 이름")
+        String name,
+
+        @Schema(description = "프로그램 설명")
+        String description,
+
+        @Schema(description = "카테고리 ID")
+        Long categoryId,
+
+        @Schema(description = "카테고리 이름")
+        String categoryName,
+
+        @Schema(description = "프로그램 시작 시간")
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd HH:mm")
+        LocalDateTime startTimestamp,
+
+        @Schema(description = "프로그램 종료 시간")
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd HH:mm")
+        LocalDateTime endTimestamp,
+
+        @Schema(description = "알림 여부")
+        boolean notificationYn
+) {
 
     public static ProgramResponse from(Program program) {
         return ProgramResponse.builder()
