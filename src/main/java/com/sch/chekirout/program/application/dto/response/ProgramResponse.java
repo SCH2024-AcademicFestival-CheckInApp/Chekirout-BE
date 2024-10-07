c0326e4a454f4a18cba8367af9466efcd2ff3e6c
Ogu1208 <kdasunb6@gmail.com>
2024-10-07 13:12:44 +0900

    refactor: Json Response 시간 포맷 변경 : yyy.MM.dd HH:mm

--- src/main/java/com/sch/chekirout/program/application/dto/response/ProgramResponse.java
+++ src/main/java/com/sch/chekirout/program/application/dto/response/ProgramResponse.java
@@ -9,36 +9,15 @@ import lombok.Getter;
 
 import java.time.LocalDateTime;
 
-@Getter
 @Builder
-@AllArgsConstructor
-public class ProgramResponse {
-
-    @Schema(description = "프로그램 ID")
-    private final String id;
-
-    @Schema(description = "프로그램 이름")
-    private final String name;
-
-    @Schema(description = "프로그램 설명")
-    private final String description;
-
-    @Schema(description = "카테고리 ID")
-    private final Long categoryId;
-
-    @Schema(description = "카테고리 이름")
-    private final String categoryName;
-
-    @Schema(description = "프로그램 시작 시간")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
-    private final LocalDateTime startTimestamp;
-
-    @Schema(description = "프로그램 종료 시간")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
-    private final LocalDateTime endTimestamp;
-
-    @Schema(description = "알림 여부")
-    private final boolean notificationYn;
+public record ProgramResponse(@Schema(description = "프로그램 ID") String id,
+                              @Schema(description = "프로그램 이름") String name,
+                              @Schema(description = "프로그램 설명") String description,
+                              @Schema(description = "카테고리 ID") Long categoryId,
+                              @Schema(description = "카테고리 이름") String categoryName,
+                              @Schema(description = "프로그램 시작 시간") @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd HH:mm") LocalDateTime startTimestamp,
+                              @Schema(description = "프로그램 종료 시간") @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd HH:mm") LocalDateTime endTimestamp,
+                              @Schema(description = "알림 여부") boolean notificationYn) {
 
     public static ProgramResponse from(Program program) {
         return ProgramResponse.builder()
