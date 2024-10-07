c1821236570cf8fea1bd71e24482f68c83f34bf4
Ogu1208 <kdasunb6@gmail.com>
2024-10-07 12:49:11 +0900

    feat: 내 프로그램 참여 내역 조회 API swagger 문서화 추가

--- src/main/java/com/sch/chekirout/participation/presentation/ParticipationRecordController.java
+++ src/main/java/com/sch/chekirout/participation/presentation/ParticipationRecordController.java
@@ -4,6 +4,9 @@ import com.sch.chekirout.participation.application.ParticipationService;
 import com.sch.chekirout.program.application.dto.response.ProgramParticipationHistories;
 import com.sch.chekirout.user.application.UserService;
 import com.sch.chekirout.user.domain.User;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -22,7 +25,11 @@ public class ParticipationRecordController {
     private final ParticipationService participationService;
     private final UserService userService;
 
-    @GetMapping()
+    @Operation(summary = "내 참여 기록 조회", description = "내가 참여한 프로그램의 기록을 조회하는 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "참여 기록 조회 성공")
+    })
+    @GetMapping
     public ResponseEntity<ProgramParticipationHistories> getMyParticipations() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String currentUsername = authentication.getName();
