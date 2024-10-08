b4e42cd6fe4008b62603bb7459be53eb3b434624
Ogu1208 <kdasunb6@gmail.com>
2024-10-08 20:35:47 +0900

    docs: stamp API에 swagger 문서화 추가

--- src/main/java/com/sch/chekirout/stampCard/presentation/StampCardAdminController.java
+++ src/main/java/com/sch/chekirout/stampCard/presentation/StampCardAdminController.java
@@ -4,6 +4,8 @@ package com.sch.chekirout.stampCard.presentation;
 import com.sch.chekirout.stampCard.application.StampCardService;
 import com.sch.chekirout.user.application.UserService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -21,7 +23,9 @@ public class StampCardAdminController {
     private final UserService userService;
 
     @Operation(summary = "모든 프로그램 참여 완료자 수 조회", description = "모든 프로그램 참여 완료자 수를 조회하는 API")
-
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "모든 프로그램 참여 완료자 수 조회 성공")
+    })
     @GetMapping("/completed")
     public ResponseEntity<Long> getCompletedPrizeCount() {
         Long completedCount = stampCardService.getCompletedPrizeCount();
