737fcc21c514a22dab5a32e76dc1804ae6fd7408
Ogu1208 <kdasunb6@gmail.com>
2024-10-10 00:38:48 +0900

    fix: WebConfig의 allowedMethods에  OPTIONS 메서드 추가

--- src/main/java/com/sch/chekirout/common/config/WebConfig.java
+++ src/main/java/com/sch/chekirout/common/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
 
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("Authorization", "Content-Type")
                 .exposedHeaders("Custom-Header")
                 .allowCredentials(true)
