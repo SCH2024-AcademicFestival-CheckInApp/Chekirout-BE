9e019afd6e62ae7dcfc92e2cf910f0016a6c5f0b
Ogu1208 <kdasunb6@gmail.com>
2024-10-14 23:51:14 +0900

    feat: 로그인한 사용자의 경품 추첨 당첨 여부 확인 API

---
+++ src/main/java/com/sch/chekirout/prizeDraw/presentation/PrizeDrawController.java
@@ -0,0 +1,36 @@
+package com.sch.chekirout.prizeDraw.presentation;
+
+import com.sch.chekirout.prizeDraw.application.PrizeDrawService;
+import com.sch.chekirout.prizeDraw.application.dto.response.PrizeWinnerResponse;
+import com.sch.chekirout.user.application.UserService;
+import com.sch.chekirout.user.domain.User;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/prize-draws")
+@Tag(name = "Prize Draw API", description = "User 경품 추첨 대상 확인 API")
+public class PrizeDrawController {
+
+    private final PrizeDrawService prizeDrawService;
+    private final UserService userService;
+
+
+    @GetMapping("/my-wins")
+    public ResponseEntity<List<PrizeWinnerResponse>> getEligableForPrizeCount() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentUsername = authentication.getName();
+        User user = userService.findUserByUsername(currentUsername);
+
+        return ResponseEntity.ok(prizeDrawService.getPrizeWinnersByStudentId(user.getUsername()));
+    }
+}
