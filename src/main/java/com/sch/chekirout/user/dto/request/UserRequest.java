12cb8ad18aa24d133e7d808d0f411e819de06be5
Kim._.jazz <jeong011010@naver.com>
2024-10-16 01:02:31 +0900

    🔨refactor: 이메일 에너테이션 수정

--- src/main/java/com/sch/chekirout/user/dto/request/UserRequest.java
+++ src/main/java/com/sch/chekirout/user/dto/request/UserRequest.java
@@ -30,10 +30,10 @@ public class UserRequest {
     @NotEmpty(message = "비밀번호는 필수 입력 항목입니다.")
     private String password;
 
-    @NotEmpty(message = "학번(username)은 필수 입력 항목입니다.")
+    @NotEmpty(message = "이메일은 필수 입력 항목입니다.")
     @Pattern(regexp = "^[A-Za-z0-9._%+-]+@sch\\.ac\\.kr$", message = "학교 이메일 형식만 사용 가능합니다.")
     private String email;
-
+    
     private String phone;
 
 
