887a1f75750aafbd7fdcace4c87c0cbafc314b60
Ogu1208 <kdasunb6@gmail.com>
2024-10-15 15:01:43 +0900

    chore: cors 오리진 설정 vercel 추가

--- src/main/java/com/sch/chekirout/common/config/WebConfig.java
+++ src/main/java/com/sch/chekirout/common/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
 
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "https://chekirout-fe.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("Authorization", "Content-Type")
                 .exposedHeaders("Authorization")
