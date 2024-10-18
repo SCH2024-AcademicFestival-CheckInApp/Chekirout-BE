8998b708ea7e582938b615cb9f4af9d9455c0c4e
Kim._.jazz <jeong011010@naver.com>
2024-10-18 17:10:16 +0900

    fix: email 주소 local -> 백 서버

--- src/main/java/com/sch/chekirout/email/service/EmailService.java
+++ src/main/java/com/sch/chekirout/email/service/EmailService.java
@@ -33,7 +33,7 @@ public class EmailService {
         String token = generateEmailVerificationToken(recipientEmail);
 
         String subject = "이메일 인증을 완료해주세요";
-        String verificationUrl = "http://localhost:8080/api/v1/auth/verify-email?token=" + token;
+        String verificationUrl = "http://dev.chekirout.com/api/v1/auth/verify-email?token=" + token;
         String message = "아래 링크를 클릭하여 이메일 인증을 완료하세요: <a href='" + verificationUrl + "'>인증 링크</a>";
 
         try {
@@ -44,7 +44,7 @@ public class EmailService {
             helper.setTo(recipientEmail);  // 수신자 이메일 설정
             helper.setSubject(subject);    // 이메일 제목 설정
             helper.setText(message, true); // HTML 형식으로 본문 설정
-            helper.setFrom("chekirout");
+            helper.setFrom("chekirout <juheun9912@gamil.com>");
 
             mailSender.send(email);  // 이메일 전송
         } catch (Exception e) {
